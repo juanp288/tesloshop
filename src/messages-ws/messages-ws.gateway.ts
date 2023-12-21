fae6172e40beddd6bd7eadedd3005c0fe7f9fa1d
Juan Pablo Victoria <jpv288@gmail.com>
2023-12-20 21:22:20 -0500

    feat: ConnectedClients
    Section 15 - Ep 210

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,9 +1,26 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+import { Socket } from 'socket.io';
 import { MessagesWsService } from './messages-ws.service';
 
 @WebSocketGateway({ cors: true })
-export class MessagesWsGateway {
+export class MessagesWsGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
   constructor(private readonly messagesWsService: MessagesWsService) {}
 
-  
+  handleConnection(client: Socket, ...args: any[]) {
+    this.messagesWsService.registerClient(client);
+    console.log(`Conectados: ${client.id}`);
+  }
+
+  handleDisconnect(client: any) {
+    console.log(
+      `Cliente desconectado: ${this.messagesWsService.getConnectedClients()}`,
+    );
+    this.messagesWsService.removeClient(client.id);
+  }
 }
